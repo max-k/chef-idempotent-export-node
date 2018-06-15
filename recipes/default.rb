5c4e1e1f75b00a18a5f16b64ded6e601f53298a4
Thomas Sarboni <max-k@post.com>
2018-06-15 13:07:06 +0200

    Access node attributes by specifying precedence level

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,22 +1,21 @@
 ruby_block "Save node attributes" do
   block do
     parent = File.join(ENV["TEMP"] || "/tmp", "kitchen")
-    node['network'].delete('counters')
-    node['memory'] = { 'total' => node['memory']['total'] }
-    node['filesystem'].each do |order, order_data|
+    node['automatic']['network'].delete('counters')
+    node['automatic']['memory'] = { 'total' => node['memory']['total'] }
+    node['automatic']['filesystem'].each do |order, order_data|
         order.each do |filesystem, filesystem_data|
             if filesystem_data.has_key?('kb_used')
-                new_fs = node['filesystem'][order][filesystem].except!('kb_used',
-                                                                       'kb_available',
-                                                                       'percent_used',
-                                                                       'inodes_used',
-                                                                       'inodes_available',
-                                                                       'inodes_percent_used')
-                node['filesystem'][order][filesystem] = new_fs
+                new_fs = node['automatic']['filesystem'][order][filesystem].except!('kb_used',
+                                                                                    'kb_available',
+                                                                                    'percent_used',
+                                                                                    'inodes_used',
+                                                                                    'inodes_available',
+                                                                                    'inodes_percent_used')
+                node['automatic']['filesystem'][order][filesystem] = new_fs
             end
         end
     end
-
     IO.write(File.join(parent, "chef_node.json"), node.to_json) if Dir::exist?(parent)
   end
 end
