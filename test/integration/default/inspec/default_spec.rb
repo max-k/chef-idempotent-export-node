3bba53b1d968368c66adb657fa3f5c3ba0dd1f08
Thomas Sarboni <max-k@post.com>
2019-01-11 14:44:33 +0100

    Fix another error in template

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -1,6 +1,6 @@
 describe file('/tmp/kitchen/chef_node.json') do
     its('type') { should eq :file }
-    its('content') { should match (/"export-node":"idempotent"/) }
+    its('content') { should match (/"recipe\[idempotent-export-node\]"/) }
     its('content') { should_not match (/"counters"/) }
     its('content') { should_not match (/"percent_used"/) }
 end
